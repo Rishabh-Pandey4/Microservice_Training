bc8420f236f3811d70ae226c56a45ead97925a60
Rishabh-Pandey4 <110027548+Rishabh-Pandey4@users.noreply.github.com>
2022-08-03 17:26:04 +0530

    Add files via upload

---
+++ Codes/Day3/Models/EmployeeDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task1.Models
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
